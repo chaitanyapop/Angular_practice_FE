bf1f6322c2d403096a3c53ac73202d98eb5e66a0
chaitanyapop <parandkarchaitanya@gmail.com>
2025-07-09 09:55:37 +0530

    added shared module and started with recipe project

--- angular_fe_practice/src/questions/questions.module.ts
+++ angular_fe_practice/src/questions/questions.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { QuestionsRoutingModule } from './questions-routing.module';
 import { ChipInputComponent } from './components/chip-input/chip-input.component';
 import { FormsModule } from '@angular/forms';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
-  declarations: [ChipInputComponent],
+  declarations: [ChipInputComponent, RecipeComponent],
   imports: [CommonModule, QuestionsRoutingModule, FormsModule],
   exports: [ChipInputComponent],
 })
